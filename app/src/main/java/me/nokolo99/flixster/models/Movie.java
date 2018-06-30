f53129b6d8a3f731f7340f02306e22108b9848ef
Nneka Okolo <nokolo@fb.com>
2018-06-29 17:32:05 -0700

    Style and Information Added

--- app/src/main/java/me/nokolo99/flixster/models/Movie.java
+++ app/src/main/java/me/nokolo99/flixster/models/Movie.java
@@ -2,13 +2,21 @@ package me.nokolo99.flixster.models;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
+@Parcel // annotation indicates class is Parcelable
 public class Movie {
+
     // values from API
-    private String title;
-    private String overview;
-    private String posterPath; // only the path
-    private String backdropPath;
+    // fields must be public for parceler
+    String title;
+    String overview;
+    String posterPath; // only the path
+    String backdropPath;
+    Double voteAverage;
+
+    // no-arg, empty constructor required for Parcreler
+    public Movie () {}
 
     // initialize from JSON data
     public Movie(JSONObject object) throws JSONException {
@@ -16,6 +24,7 @@ public class Movie {
         overview = object.getString("overview");
         posterPath = object.getString("poster_path");
         backdropPath = object.getString("backdrop_path");
+        voteAverage = object.getDouble("vote_average");
     }
 
     public String getTitle() {
@@ -32,5 +41,9 @@ public class Movie {
 
     public String getBackdropPath() {
         return backdropPath;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
     }
 }
